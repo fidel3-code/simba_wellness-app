eccd982efad8d0c60fe5325eba945fbab6e2384f
lonewolf-sar <karbolojoshua@gmail.com>
2026-04-07 13:40:56 +0300

    Updated the app

--- app/src/main/java/com/fidel/simba_wellnessapp/MainActivity.kt
+++ app/src/main/java/com/fidel/simba_wellnessapp/MainActivity.kt
@@ -54,5 +54,47 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        val hydration=findViewById<Button>(R.id.hydration)
+
+        hydration.setOnClickListener {
+
+            val hydrationIntent= Intent(applicationContext, HydrationAlert::class.java)
+
+            startActivity(hydrationIntent)}
+
+
+
+        val exercise=findViewById<Button>(R.id.exercise)
+
+        exercise.setOnClickListener {
+            val exerciseIntent= Intent(applicationContext, StartExercise::class.java)
+            startActivity(exerciseIntent)
+        }
+
+
+        val motivation=findViewById<Button>(R.id.motivation)
+
+        motivation.setOnClickListener {
+            val motivationIntent= Intent(applicationContext,DailyMotivation::class.java)
+            startActivity(motivationIntent)
+        }
+
+        val goals=findViewById<Button>(R.id.goals)
+
+        goals.setOnClickListener {
+            val goalsIntent= Intent(applicationContext, WeeklyGoals::class.java)
+            startActivity(goalsIntent)
+        }
+
+        val progress=findViewById<Button>(R.id.progress)
+
+        progress.setOnClickListener {
+            val progressIntent= Intent(applicationContext, CheckProgress::class.java)
+            startActivity(progressIntent)
+        }
+
+
+
+
     }
 }
